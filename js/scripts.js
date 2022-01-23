15e68704e8edcb4074d6e3f14188f592194ddcd0
grantkee <grant.d.kee@gmail.com>
2022-01-23 17:32:37 -0600

    update names

--- js/scripts.js
+++ js/scripts.js
@@ -184,7 +184,7 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Grant and Anna are getting married!",
 
             // Event start date
             start: new Date('Nov 27, 2017 10:00'),
